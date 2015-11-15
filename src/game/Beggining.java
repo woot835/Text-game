0685c401b036874e276ae496c41a1a6063b1dfa7
Meelis Hass <mhass@itcollege.ee>
2015-11-15 10:38:43 +0200

    First Commit of the project thingy.                          Added Main file in Start                          need to work on beginning

---
+++ src/game/Beggining.java
@@ -0,0 +1,13 @@
+package game;
+
+public class Beggining {
+
+	
+	public static void main(String[] args) {
+		int start = (int) ((Math.random()*6) + 1);
+		System.out.println(start);
+		
+		
+		
+	}
+}
---
+++ src/game/Start.java
@@ -0,0 +1,17 @@
+package game;
+import library.TextIO;
+public class Start {
+
+public static void main(String[] args) {
+	
+	System.out.println("Welcome to this text based game");
+	System.out.println("Can I have your name?");
+	String name = TextIO.getlnString();
+	System.out.println("Now %d, You will begin your adventure at %s");
+	
+	
+	
+	
+}
+
+}

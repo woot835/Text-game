956090db009fa8ff74e58fc821be583fd4da1042
mhass <mhass@itcollege.ee>
2015-11-16 14:10:35 +0200

    Added the main 5 beginnings as String algus

--- src/game/Start.java
+++ src/game/Start.java
@@ -7,6 +7,29 @@ public class Start {
 		System.out.println("Welcome to this text based game made by me, Meelis Hass from C11");
 		System.out.println("Can I have your name?");
 		String name = TextIO.getlnString();
+
+		if (beginning.seed = 0) {
+			String algus = "test1"
+
+		} if (beginning.seed = 1) {
+			String algus = "test2"
+
+		}if (beginning.seed = 2) {
+			String algus = "test3"
+
+		}if (beginning.seed = 3) {
+			String algus = "test4"
+
+		}if (beginning.seed = 4) {
+			String algus = "test5"
+
+		}if (beginning.seed = 5) {
+			String algus = "test6"
+
+		} else {
+			System.out.println("You broke the game gj");
+		}
+
 		System.out.println("Now"+ name +", You will begin your adventure at ");
 
 		Beginning beginning = new Beginning();
